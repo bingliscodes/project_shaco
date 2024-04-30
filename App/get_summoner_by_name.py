02c2a53636da4f0d815017d4844d3231fb162822
Benjamin Inglis <bjammin134@gmail.com>
2024-04-29 19:20:52 -0700

    Added .gitignore

--- App/get_summoner_by_name.py
+++ App/get_summoner_by_name.py
@@ -2,7 +2,7 @@ import requests
 
 api_key = 'RGAPI-f01f9637-f7a9-477e-a710-6950923641c5'
 tagLine = 'NA1'
-gameName = 'Protein Shako'
+gameName = 'ye qiue'
 api_url = 'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/'
 request_url = api_url + gameName + '/' + tagLine + '?api_key=' + api_key
 res = requests.get(request_url)
