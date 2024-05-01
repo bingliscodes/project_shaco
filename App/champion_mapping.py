5bcefe0b2dba42c824c70b731fadbbcd83c22c54
Benjamin Inglis <bjammin134@gmail.com>
2024-05-01 16:08:48 -0700

    Organizational changes.
    Re-organized python modules into a src directory and a riot_api_calls submodule.
    
    Also began implementing the second part of the get_match_data module.

--- App/champion_mapping.py
+++
@@ -1,9 +0,0 @@
-#champion_mapping.py
-#TODO: Figure out how to map the id to champion name (may not be a necessary step for now since shaco's id is static)
-import json
-import requests
-
-ddragon_url = 'https://ddragon.leagueoflegends.com/cdn/14.8.1/data/en_US/champion.json'
-
-champs_json = requests.get(ddragon_url)
-print(champs_json)
